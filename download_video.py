6c24ddca65ab460515ae029fa10aeb44fb8bc696
ron404conde <sollezarondel@gmail.com>
2021-03-14 01:15:53 +0800

    Initial commit

---
+++ download_video.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+
+print("Please choice: ")
+print("1 for VIDEO")
+print("2 for MP3")
+print("===============")
+download_mode = input("Download: ")
+
+video_link = input("Enter Video URL: ")
+video = YouTube(video_link)
+
+if(download_mode == "1"):
+    stream_video = video.streams.get_highest_resolution()
+    stream_video.download()
+elif(download_mode == "2"):
+    stream_video = video.streams.get_audio_only()
+    stream_video.download()
+else:
+    print("Incorect choice: 1 for video and 2 for mp3")
+
+
+# stream_video = video.streams.get_highest_resolution()
+# stream_video.download()
